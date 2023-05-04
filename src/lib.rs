91d85560b8cd5e3ed6ced793aac616be1ba753bb
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-05-04 13:24:27 +0200

    feat: ⚡️ Code cleanup and improve performance

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-// Wokwi Custom Chips with Rust
-//
-// Very rough prototype by Uri Shaked
-//
-// Look at chipInit() at the bottom, and open Chrome devtools console to see the debugPrint().
-
 use std::ffi::{c_void, CString};
-use wokwi_chip_ll::{debugPrint, i2cInit, pinInit, I2CConfig, I2CDevId, INPUT};
+use wokwi_chip_ll::{debugPrint, i2cInit, pinInit, I2CConfig, INPUT};
 
 struct Chip {
-    _id: I2CDevId,
     internal_address: Register,
     state: State,
 }
@@ -21,21 +14,39 @@ enum State {
     ExpectingWriteByte2,
 }
 
+#[derive(Clone, Copy)]
 enum Register {
     WhoAmI = 0x75,
     Uninitialized = 0xFF,
 }
 
+impl Register {
+    fn from_address(address: u8) -> Register {
+        match address {
+            0x75 => Register::WhoAmI,
+            _ => Register::Uninitialized,
+        }
+    }
+}
+
 const ADDRESS: u32 = 0x68;
+
+const PRODUCT_ID_HI: u8 = 0x67;
+const PRODUCT_ID_LO: u8 = 0x00;
 
 static mut CHIP_VEC: Vec<Chip> = Vec::new();
 
 #[no_mangle]
 pub unsafe extern "C" fn chipInit() {
-    debugPrint(CString::new("Hello Rust!").unwrap().into_raw());
+    debugPrint(CString::new("Initializing IMC42670P").unwrap().into_raw());
+    let chip = Chip {
+        internal_address: Register::Uninitialized,
+        state: State::ExpectingConnect,
+    };
+    CHIP_VEC.push(chip);
 
     let i2c_config: I2CConfig = I2CConfig {
-        user_data: std::ptr::null::<c_void>(),
+        user_data: (CHIP_VEC.len() - 1) as *const c_void,
         address: ADDRESS,
         scl: pinInit(CString::new("SCL").unwrap().into_raw(), INPUT),
         sda: pinInit(CString::new("SDA").unwrap().into_raw(), INPUT),
@@ -44,18 +55,13 @@ pub unsafe extern "C" fn chipInit() {
         write: on_i2c_write as *const c_void,
         disconnect: on_i2c_disconnect as *const c_void,
     };
+    i2cInit(&i2c_config);
 
-    let chip = Chip {
-        _id: i2cInit(&i2c_config),
-        internal_address: Register::Uninitialized,
-        state: State::ExpectingConnect,
-    };
-    CHIP_VEC.push(chip);
     debugPrint(CString::new("Chip initialized!").unwrap().into_raw());
 }
 
 pub unsafe fn on_i2c_connect(user_ctx: *const c_void, address: u32, read: bool) -> bool {
-    let msg: String = format!("on_i2c_connect: add: {}, read: {}", address, read);
+    let msg: String = format!("on_i2c_connect: address: {}, read: {}", address, read);
     debugPrint(CString::new(msg).unwrap().into_raw());
     let chip: &mut Chip = &mut CHIP_VEC[user_ctx as usize];
     if read {
@@ -73,18 +79,17 @@ pub unsafe fn on_i2c_read(user_ctx: *const c_void) -> u8 {
     match chip.state {
         State::ExpectingReadByte1 => match chip.internal_address {
             Register::WhoAmI => {
-                debugPrint(CString::new("WhoAmI").unwrap().into_raw());
                 chip.state = State::ExpectingReadByte2;
-                return 0x67;
+                return PRODUCT_ID_HI;
             }
             _ => {
-                debugPrint(CString::new("Other").unwrap().into_raw());
                 chip.state = State::ExpectingConnect;
             }
         },
         State::ExpectingReadByte2 => match chip.internal_address {
             Register::WhoAmI => {
                 chip.state = State::ExpectingConnect;
+                return PRODUCT_ID_LO;
             }
             _ => {
                 chip.state = State::ExpectingConnect;
@@ -102,13 +107,10 @@ pub unsafe fn on_i2c_write(user_ctx: *const c_void, data: u8) -> bool {
     debugPrint(CString::new(msg).unwrap().into_raw());
 
     let chip: &mut Chip = &mut CHIP_VEC[user_ctx as usize];
+    chip.internal_address = Register::from_address(data);
 
     match chip.state {
         State::ExpectingWriteByte1 => {
-            chip.internal_address = match data {
-                0x75 => Register::WhoAmI,
-                _ => Register::Uninitialized,
-            };
             chip.state = State::ExpectingWriteByte2;
         }
         _ => {
